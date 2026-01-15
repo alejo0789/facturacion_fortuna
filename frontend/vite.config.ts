92cb5b23b96b0ef9841e6dabc62e5e59a3e74fb2
Alejandro Carvajal Gonzalez <alejandro.carvajal@LAFORTUNA.LOCAL>
2026-01-15 08:59:56 -0500

    adding fecha retiro fecha salida y ref contrato

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,15 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/facturacion_ia/',
+  base: '/',
   server: {
     host: '0.0.0.0',
-    port: 5173,
-    hmr: {
-      host: '192.168.2.91',
-      port: 5173,
-      protocol: 'ws'
-    }
+    port: 5173
   },
   plugins: [react()]
 })
