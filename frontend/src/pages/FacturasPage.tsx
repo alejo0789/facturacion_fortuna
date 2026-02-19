6d7289143f66b2f8153c6f1ac7179cb9e5cfd9e7
Alejandro Carvajal Gonzalez <alejandro.carvajal@LAFORTUNA.LOCAL>
2026-02-19 14:39:30 -0500

    Remove direct insertion warning from Causacion Manager preview

--- frontend/src/pages/FacturasPage.tsx
+++ frontend/src/pages/FacturasPage.tsx
@@ -3085,21 +3085,8 @@ export default function FacturasPage() {
                                         </div>
                                     </div>
 
-                                    {/* Warning */}
-                                    <div className="bg-yellow-50 border border-yellow-200 rounded-lg p-4">
-                                        <div className="flex items-start gap-3">
-                                            <svg className="w-5 h-5 text-yellow-600 mt-0.5" fill="currentColor" viewBox="0 0 20 20">
-                                                <path fillRule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clipRule="evenodd" />
-                                            </svg>
-                                            <div>
-                                                <div className="font-medium text-yellow-800">Nota Importante</div>
-                                                <p className="text-sm text-yellow-700 mt-1">
-                                                    La funcionalidad de inserción directa en Manager ERP está pendiente de implementación.
-                                                    Por ahora, puede descargar el archivo Excel usando el botón "Generar Archivo Plano".
-                                                </p>
-                                            </div>
-                                        </div>
-                                    </div>
+
+
                                 </div>
                             )}
                         </div>
